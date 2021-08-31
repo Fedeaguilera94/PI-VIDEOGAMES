05b2b8ddf21efab8f6c4c6c42cc7babc2e7da1ff
Fedeaguilera94 <aguilerafederico94@gmail.com>
2021-08-31 09:12:28 -0300

    init

---
+++ api/src/models/Genre.js
@@ -0,0 +1,17 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+    // defino el modelo
+    sequelize.define("genre", {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4
+        },
+    });
+  };
+  
---
+++ api/src/models/Videogame.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("videogame", {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    releaseDate: {
+      type: DataTypes.STRING,
+    },
+    rating: {
+      type: DataTypes.REAL,
+    },
+
+    platforms: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};

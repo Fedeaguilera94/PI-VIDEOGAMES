e1a4973a637cbe38ac3b9a289886428882a69076
Fedeaguilera94 <aguilerafederico94@gmail.com>
2021-10-29 00:30:38 -0300

    deploy1

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const { conn } = require("./src/db.js");
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   // estaba en true
-  server.listen(3001, () => {
+  server.listen(process.env.PORT, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console INVOCAR LA FUNCION DE CREAR GENEROS
   });
 });
